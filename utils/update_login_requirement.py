503760eb64b44f8b617f0a13124869daf84f662f
xolarvill <since1963@qq.com>
2025-08-01 11:31:13 +0800

    fix: update target and replacement values in update_login_requirement function

--- utils/update_login_requirement.py
+++ utils/update_login_requirement.py
@@ -1,5 +1,5 @@
-target = '},{"key":"mockup-list"'
-to = '},{"key":"need-login","value":"true"},{"key":"mockup-list"'
+target = '"domDataset":[{"'
+to = '"domDataset":[{"key":"need-login","value":"true"},{"'
 
 def update_login_requirment(t: str) -> str:
     result = t.replace(target, to)
